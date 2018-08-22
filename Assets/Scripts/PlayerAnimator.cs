7ad1363b820ef334220b36dd9c9953326c747422
qinflying <qinflying@outlook.com>
2018-08-22 23:56:31 +0800

    控制角色移动和run
    Signed-off-by: qinflying <qinflying@outlook.com>

---
+++ Assets/Scripts/PlayerAnimator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+    private Animator m_Animator;
+    private int m_SpeedID;
+    private int m_IsPlusSpeedID;
+    // Use this for initialization
+    void Start()
+    {
+        m_Animator = GetComponent<Animator>();
+        m_SpeedID = Animator.StringToHash("Speed");
+        m_IsPlusSpeedID = Animator.StringToHash("IsPlusSpeed");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float v = Input.GetAxis("Vertical");
+        m_Animator.SetFloat(m_SpeedID, v);
+        if (Input.GetKeyDown(KeyCode.LeftShift)) {
+            m_Animator.SetBool(m_IsPlusSpeedID, true);
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftShift)) {
+            m_Animator.SetBool(m_IsPlusSpeedID, false);
+        }
+    }
+}
