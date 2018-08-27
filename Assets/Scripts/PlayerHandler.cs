ee9b3ce70c96a7b730ffeac6d6feb1fab73895d0
qinflying <qinflying@outlook.com>
2018-08-27 23:32:17 +0800

    扛Logg
    Signed-off-by: qinflying <qinflying@outlook.com>

---
+++ Assets/Scripts/PlayerHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHandler : MonoBehaviour {
+    public GameObject m_PlayerLog;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider collision) {
+        if (collision.tag == "Log") {
+            Destroy(collision.gameObject);
+            CarryMood();
+        }
+    }
+
+    void CarryMood() {
+        m_PlayerLog.SetActive(true);
+    }
+}
