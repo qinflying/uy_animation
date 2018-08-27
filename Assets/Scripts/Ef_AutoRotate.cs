d0cdc03b9e4f207fcf0a0fe3b7973c738ca72a05
qinflying <qinflying@outlook.com>
2018-08-27 23:16:54 +0800

    场景布置旋转Logg
    Signed-off-by: qinflying <qinflying@outlook.com>

---
+++ Assets/Scripts/Ef_AutoRotate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ef_AutoRotate : MonoBehaviour
+{
+    public float m_RotateSpeed = 90f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up * m_RotateSpeed * Time.deltaTime, Space.World);
+    }
+}
